de7a7ef171a77414bd9af3991fa761cdf14221a5
Damon <zhuowangyi@163.com>
2020-03-06 13:55:43 +0800

    更换跳转域名

--- src/utils/testingEquipment.js
+++ src/utils/testingEquipment.js
@@ -3,6 +3,6 @@
 // PC端 移动端 跳转
 var is_mobi = navigator.userAgent.toLowerCase().match(/(ipod|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
 if (is_mobi) {
-    window.location.href="https://m.aitime.credit/#/home" 
+    window.location.href="https://m.heyfish.com/#/home" 
 } else {
 }
