c287cf4dfcfd3124e34c6feb8512ce047aa118d5
Damon <zhuowangyi@163.com>
2020-04-07 17:34:30 +0800

    登陆按钮暂时隐藏

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -3,7 +3,7 @@
     <div class="wrap">
       <div class="left-area">
         <img class="img" src="../../../public/images/header-logo@2x.png" width="144px;height:28px;" />
-        <a class="login-btn" href="javascript:;">{{currentData[language][3]}}</a>
+        <!-- <a class="login-btn" href="javascript:;">{{currentData[language][3]}}</a> -->
       </div>
       <div class="middle-area"></div>
       <div class="right-area">
